339e9dc563983abc17175b2a50cc74897c357f39
slionel185 <slionel1850@gmail.com>
2021-01-09 21:43:39 -0800

    Build

---
+++ build/static/js/main.b0d154fd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);var c=n(1),i=(n(0),n(3)),s=n.n(i),o=(n(9),n.p+"static/media/comingSoon.6823abb9.png"),a=function(){return Object(c.jsx)("img",{className:"pic",src:o,alt:"should be a pic here"})};s.a.render(Object(c.jsx)(a,{}),document.getElementById("root"))},9:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.b0d154fd.chunk.js.map
--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import './main.css'
+import background from './comingSoon.png'
+
 const App = () => {
     return (
-        <h1>Hey</h1>
+        <img className="pic" src={background} alt="should be a pic here"/>
     )
 }
 
